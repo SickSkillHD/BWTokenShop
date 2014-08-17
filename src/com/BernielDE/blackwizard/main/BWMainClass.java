111e948a69af2e13026d87ce6e9ccf64feb58546
thedaniel99m <thedaniel99m@gmail.com>
2014-08-17 17:31:47 +0200

    2. Commit

--- src/com/BernielDE/blackwizard/main/BWMainClass.java
+++ src/com/BernielDE/blackwizard/main/BWMainClass.java
@@ -3,5 +3,10 @@ package com.BernielDE.blackwizard.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BWMainClass extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("[BWTokenShop] Plugin geladen..");
+	}
 
 }
